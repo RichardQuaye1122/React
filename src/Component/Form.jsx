e2c3bea9f30209818ee5024b70797566e752e90d
RichardQuaye1122 <quayerichard1122@gmail.com>
2025-11-24 19:09:27 +0000

    React Week 5 Assignment

---
+++ src/Component/Form.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from "react";
+
+const Form = () => {
+  const[userName, setUserName] = useState("")
+  const[userEmail, setUserEmail] = useState("")
+
+  const handleUserName = (e) =>{
+    setUserName(e.target.value)
+  }
+
+  const handleUserEmail = (e) => {
+    setUserEmail(e.target.value)
+  }
+
+  const submit = (e) => {
+    console.log(userName)
+    console.log(userEmail)
+    e.preventDefault()
+    setUserName("")
+    setUserEmail("")
+  }
+
+  return (
+    <>
+      <form onSubmit={submit}>
+        <label>Name</label>
+        <input type="text" value={userName} onChange={handleUserName} />
+
+        <label>Email</label>
+        <input type="text" value={userEmail} onChange={handleUserEmail}/>
+
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
+
+export default Form
